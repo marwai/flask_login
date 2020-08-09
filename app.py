f1fdb2aa1e9168b45f9fad8f64527c67f31360f9
marwai <mtse@spartaglobal.com>
2020-08-09 12:29:15 +0100

    updating flask project - welcome page to be first page

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def login_required(f):
             return f(*args, **kwargs)
         else:
             flash('You need to login first.')
-            return redirect(url_for('login'))
+            return redirect(url_for('welcome'))
     return wrap
 
 # use decorators to link the function to a url
